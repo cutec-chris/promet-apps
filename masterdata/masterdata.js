0416ba2acaa698e992912b11fff70d6f9ae5cb88
Chris <ulrich@tcs-germany.de>
2018-01-10 08:54:09 +0100

    Masterdata:fix Header

--- masterdata/masterdata.js
+++ masterdata/masterdata.js
@@ -1,10 +1,10 @@
 var Masterdata;
 dhtmlxEvent(window,"load",function(){
   Masterdata = newPrometList('masterdata','Stammdaten');
-  Masterdata.Grid.setHeader(["Kurztext","Nummer","Version","Status","Kategorie"]);
-  Masterdata.Grid.setColumnIds('SHORTTEXT,ID,VERSION,STATUS,CATEGORY')
+  Masterdata.Grid.setHeader(["Nummer","Kurztext","Version","Status","Kategorie"]);
+  Masterdata.Grid.setColumnIds('ID,SHORTTEXT,VERSION,STATUS,CATEGORY')
   Masterdata.Grid.setColTypes("ro,ro,ro,ro,ro");
-  Masterdata.Grid.attachHeader("#text_filter,#text_filter,#text_filter,#select_filter,");
-  Masterdata.Grid.setInitWidths('*,70,150,*');
+  Masterdata.Grid.attachHeader("#text_filter,#text_filter,#text_filter,#select_filter,#text_filter");
+  Masterdata.Grid.setInitWidths('150,*,70,100,150');
   Masterdata.Grid.init();
 });
