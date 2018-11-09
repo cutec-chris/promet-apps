b6f5099438f7e4654b665189945ba97f5d36765f
Chris <ulrich@tcs-germany.de>
2018-11-09 15:16:34 +0100

    cache only selected files

--- updatechecker.js
+++ updatechecker.js
@@ -1,43 +1,67 @@
-//This is the "Offline copy of pages" service worker
+var CACHE = 'avamm-precache';
+var precacheFiles = [
+    'index.html',
+    './', // Alias for index.html
+    'index.css',
+    'manifest.json',
+    'appbase/appbase.js',
+    'appbase/startpage.js',
+    'appbase/dhtmlx/dhtmlx.js',
+    'appbase/dhtmlx/dhtmlx.css',
+    'appbase/dhtmlx/fonts/font_awesome/css/font-awesome.min.css',
+    'appbase/images/world_icon144.png'
+    'appbase/images/world_icon192.png'
+    ];
 
-//Install stage sets up the index page (home page) in the cache and opens a new cache
-self.addEventListener('install', function(event) {
-  var indexPage = new Request('index.html');
-  event.waitUntil(
-    fetch(indexPage).then(function(response) {
-      return caches.open('pwabuilder-offline').then(function(cache) {
-        console.log('[PWA Builder] Cached index page during Install'+ response.url);
-        return cache.put(indexPage, response);
-      });
+//Install stage sets up the cache-array to configure pre-cache content
+self.addEventListener('install', function(evt) {
+  console.log('The update service worker is being installed.');
+  evt.waitUntil(precache().then(function() {
+    console.log('Skip waiting on install');
+    return self.skipWaiting();
   }));
 });
 
-//If any fetch fails, it will look for the request in the cache and serve it from there first
-self.addEventListener('fetch', function(event) {
-  var updateCache = function(request){
-    return caches.open('pwabuilder-offline').then(function (cache) {
-      return fetch(request).then(function (response) {
-        console.log('[PWA Builder] add page to offline'+response.url)
-        return cache.put(request, response);
-      });
+
+//allow sw to control of current page
+self.addEventListener('activate', function(event) {
+  console.log('Claiming clients for current page');
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(evt) {
+  console.log('The service worker is serving the asset.'+ evt.request.url);
+  evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));
+  evt.waitUntil(update(evt.request));
+});
+
+
+function precache() {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.addAll(precacheFiles);
+  });
+}
+
+function fromCache(request) {
+  //we pull files from the cache first thing so we can show them fast
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
     });
-  };
+  });
+}
 
-  event.waitUntil(updateCache(event.request));
+function update(request) {
+  //this is where we call the server to get the newest version of the
+  //file to use the next time we show view
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    });
+  });
+}
 
-  event.respondWith(
-    fetch(event.request).catch(function(error) {
-      console.log( '[PWA Builder] Network request Failed. Serving content from cache: ' + error );
-
-      //Check to see if you have it in the cache
-      //Return response
-      //If not in the cache, then return error page
-      return caches.open('pwabuilder-offline').then(function (cache) {
-        return cache.match(event.request).then(function (matching) {
-          var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
-          return report
-        });
-      });
-    })
-  );
-})
+function fromServer(request){
+  //this is the fallback if it is not in the cache to go to the server and get it
+  return fetch(request).then(function(response){ return response});
+}
